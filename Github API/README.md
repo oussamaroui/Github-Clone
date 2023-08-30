f1b714d9099c30a604234cee5181acc180056a9a
Oussama Roui <62429948+oussamaroui@users.noreply.github.com>
2023-08-31 01:13:22 +0200

    GitHub Clone with its Functions
    Clone GitHub Profile Page with its Functions

---
+++ Github API/README.md
@@ -0,0 +1,1 @@
+This repository does not include the node_modules folder, it must be added to it in order for it to work without problems.
---
+++ Github API/src/App.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from 'react-dom/client'
+import Index from './index.jsx' ;
+
+ReactDOM.createRoot(document.getElementById('root')).render(<Index />);
+
+
